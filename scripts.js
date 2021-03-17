6cb25320f36af27eaa32aa1d58912905dfd49cd9
Vince Colletti <vince.colletti.1001@gmail.com>
2021-03-17 15:40:07 -0700

    created timer and period system for click game

--- scripts.js
+++ scripts.js
@@ -2,8 +2,15 @@ $(document).ready(function(){
 
   // if the email address is blank, nothing happens when subscribe button is clicked. otherwise, subscribe box dissappears when you close the modal
   $('#subscribe-button').click(function(){
+    let valid = false
     let typedText = $('#subscribe-email').val()
-    if(typedText === ''){
+    for(let i=0; i<typedText.length; i++){
+      if(typedText[i] === '@'){
+        valid = true
+      }
+    }
+
+    if(typedText === '' || valid === false){
       $('#modal-title').text("Something went wrong")
       $('#modal-body').text('Please enter a valid email address')
     } else{
@@ -26,4 +33,59 @@ $(document).ready(function(){
     piratesScore++
     $('#pirates-score').text(piratesScore)
   })
+
+  // script to end game
+  let timer = 15
+  let period = 1
+  $('#period-count').text('1st period')
+  $('#count-down-timer').text('0:'+timer+' remaining')
+  let timeRemaining = setInterval(function(){
+    if(timer>0){
+      timer--
+    } else{
+      if(period === 4){
+        clearInterval(timeRemaining)
+        if(ninjasScore === piratesScore){
+          alert('Game Over!\nThe game is tied at '+ninjasScore+' points')
+        } else{
+          if(ninjasScore > piratesScore){
+            alert('Game Over!\nThe score is '+ninjasScore+' to '+piratesScore+'\n Ninjas Win!!!')
+          } else{
+            alert('Game Over!\nThe score is '+piratesScore+' to '+ninjasScore+'\n Pirates Win!!!')
+          }
+        }
+      } else{
+        if(period === 3){
+          alert('The third period is over')
+          period++
+          $('#period-count').text('4th period')
+          timer = 15
+        } else{
+          if(period === 2){
+            alert('it\'s Halftime!')
+            period++
+            $('#period-count').text('3rd period')
+            timer = 15
+          } else{
+            alert('The 1st period is over')
+            period++
+            $('#period-count').text('2nd period')
+            timer = 15
+          }
+        }
+      }
+    }
+
+
+
+
+
+
+    
+    if(timer>9){
+      $('#count-down-timer').text('0:'+timer+' remaining')
+    } else{
+      $('#count-down-timer').text('0:0'+timer+' remaining')
+    }
+  }, 1000)
 })
