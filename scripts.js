9f1aaaa1aae63e1d92f04515b3fb17a8f01eb567
Vince Colletti <vince.colletti.1001@gmail.com>
2021-03-17 13:38:22 -0700

    created subscribe modal and scripts, created click game increments

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-  console.log('Hello world!')
+
+  // if the email address is blank, nothing happens when subscribe button is clicked. otherwise, subscribe box dissappears when you close the modal
+  $('#subscribe-button').click(function(){
+    let typedText = $('#subscribe-email').val()
+    if(typedText === ''){
+      $('#modal-title').text("Something went wrong")
+      $('#modal-body').text('Please enter a valid email address')
+    } else{
+      $('#modal-title').text("Thank you for subscribing to the DSNN Newsletter")
+      $('#modal-body').text('You will recieve email updates at '+typedText)
+      $('#modal-close-button').click(function(){
+        $('#subscribe-block').hide()
+      })
+    }
+  })
+
+  // Scripts to increment the scores
+  let ninjasScore = 0;
+  let piratesScore = 0;
+  $('#ninja-image, #ninjas-score').click(function(){
+    ninjasScore++
+    $('#ninjas-score').text(ninjasScore)
+  })
+  $('#pirate-image, #pirates-score').click(function(){
+    piratesScore++
+    $('#pirates-score').text(piratesScore)
+  })
 })
