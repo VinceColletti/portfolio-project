c3d11936aea35a21d863d7b6ab59fcb3f1d9eb39
Vince Colletti <vince.colletti.1001@gmail.com>
2021-03-22 16:47:19 -0700

    cleaned up JS code

--- scripts.js
+++ scripts.js
@@ -1,121 +1,7 @@
 $(document).ready(function(){
-
-  // if the email address is blank, nothing happens when subscribe button is clicked. otherwise, subscribe box dissappears when you close the modal
-  $('#subscribe-button').click(function(){
-    let valid = false
-    let typedText = $('#subscribe-email').val()
-    for(let i=0; i<typedText.length; i++){
-      if(typedText[i] === '@'){
-        valid = true
-      }
-    }
-
-    if(typedText === '' || valid === false){
-      $('#modal-title').text("Something went wrong")
-      $('#modal-body').text('Please enter a valid email address')
-    } else{
-      $('#modal-title').text("Thank you for subscribing to the DSNN Newsletter")
-      $('#modal-body').text('You will recieve email updates at '+typedText)
-      $('#modal-close-button').click(function(){
-        $('#subscribe-block').hide()
-      })
-    }
-  })
-
-  // Scripts to increment the scores while game is not running
-  let ninjasScore = 0;
-  let piratesScore = 0;
-  $('#ninja-image, #ninjas-score').click(function(){
-    ninjasScore++
-    $('#ninjas-score').text(ninjasScore)
-  })
-  $('#pirate-image, #pirates-score').click(function(){
-    piratesScore++
-    $('#pirates-score').text(piratesScore)
-  })
-
-  // Click game script
-  let timer = 15
-  let period = 1
-  $('#period-count').text('1st period')
-  $('#count-down-timer').text('0:'+timer+' remaining')
-  $('#start-game-button').click(function(){
-    $('#start-game-button').hide()
-
-    // initialize scores
-    let ninjasScore = 0;
-    let piratesScore = 0;
-    $('#ninjas-score').text(ninjasScore)
-    $('#pirates-score').text(piratesScore)
-    $('#ninja-image, #ninjas-score').click(function(){
-      ninjasScore++
-      $('#ninjas-score').text(ninjasScore)
-    })
-    $('#pirate-image, #pirates-score').click(function(){
-      piratesScore++
-      $('#pirates-score').text(piratesScore)
-    })
-    
-    // get timer going
-    let timeRemaining = setInterval(function(){
-    if(timer>0){
-      timer--
-    } else{
-      //end game
-      if(period === 4){
-        clearInterval(timeRemaining)
-        if(ninjasScore === piratesScore){
-          alert('Game Over!\nThe game is tied at '+ninjasScore+' points')
-        } else{
-          if(ninjasScore > piratesScore){
-            alert('Game Over!\nThe score is '+ninjasScore+' to '+piratesScore+'\n Ninjas Win!!!')
-          } else{
-            alert('Game Over!\nThe score is '+piratesScore+' to '+ninjasScore+'\n Pirates Win!!!')
-          }
-        }
-        // keep rolling through the periods
-      } else{
-        if(period === 3){
-          alert('The third period is over')
-          period++
-          $('#period-count').text('4th period')
-          timer = 15
-        } else{
-          if(period === 2){
-            alert('it\'s Halftime!')
-            period++
-            $('#period-count').text('3rd period')
-            timer = 15
-          } else{
-            alert('The 1st period is over')
-            // $('#staticBackdrop').modal('show')
-            period++
-            $('#period-count').text('2nd period')
-            timer = 15
-          }
-        }
-      }
-    } 
-
-    // make sure timer displays properly  
-    if(timer>9){
-      $('#count-down-timer').text('0:'+timer+' remaining')
-    } else{
-      $('#count-down-timer').text('0:0'+timer+' remaining')
-    }
-    }, 1000)
-  })
-
-
-  // how to show the modal. add class of 'show'
-  $('#test-button').click(function(){
-    $('#staticBackdrop').addClass('show')
-  })
-
-
-  // theSportsDb
+    // theSportsDb
   // NFL league ID: 4391
-  // NCAA Basketball Mens ID: 4607
+  // NCAA Basketball Mens league ID: 4607
   $.ajax({
     // last 15 events for NCAA basketball
     url: 'https://www.thesportsdb.com/api/v1/json/1/eventspastleague.php?id=4607',
@@ -124,15 +10,134 @@ $(document).ready(function(){
       // console.log(data)
       $('#live-score-text').text('March Madness')
       for(i=0; i<4; i++){
-        let homeTeam = data.events[i].strHomeTeam
-        let homeScore = data.events[i].intHomeScore
-        let awayTeam = data.events[i].strAwayTeam
-        let awayScore = data.events[i].intAwayScore
-        let scoreText = '<p>'+homeTeam+'► '+homeScore+'</p><p>'+awayTeam+'► '+awayScore+'</p>'
-        $('#score-'+i).html(scoreText)
+        let homeTeam = data.events[i].strHomeTeam;
+        let homeScore = data.events[i].intHomeScore;
+        let awayTeam = data.events[i].strAwayTeam;
+        let awayScore = data.events[i].intAwayScore;
+        let scoreText = '<p>'+homeTeam+'► '+homeScore+'</p><p>'+awayTeam+'► '+awayScore+'</p>';
+        $('#score-'+i).html(scoreText);
       }
     }
-    
-  })
+  });
 
-})
+    // How to show the modal
+    // $('#staticBackdrop').modal('show')
+
+  // if the email address is blank, nothing happens when subscribe button is clicked. otherwise, subscribe box dissappears when you close the modal
+  $('#subscribe-button').click(function(){
+    let valid = false;
+    let typedText = $('#subscribe-email').val();
+    // check to see if email is "valid"
+    for(let i=0; i<typedText.length; i++){
+      if(typedText[i] === '@'){
+        valid = true;
+      };
+    };
+    // show the modal if there is a "valid" email
+    if(typedText === '' || valid === false){
+      $('#modal-title').text("Something went wrong");
+      $('#modal-body').text('Please enter a valid email address');
+    }
+    else{
+      $('#modal-title').text("Thank you for subscribing to the DSNN Newsletter");
+      $('#modal-body').text('You will recieve email updates at '+typedText);
+      $('#modal-close-button').click(function(){
+        $('#subscribe-block').hide();
+      });
+    };
+  });
+
+  // Script to increment the scores while game is not running
+  let ninjasScore = 0;
+  let piratesScore = 0;
+  $('#ninja-image, #ninjas-score').click(function(){
+    ninjasScore++;
+    $('#ninjas-score').text(ninjasScore);
+  });
+  $('#pirate-image, #pirates-score').click(function(){
+    piratesScore++;
+    $('#pirates-score').text(piratesScore);
+  });
+
+  // script for click game
+  let timer = 15;
+  let period = 4;
+  $('#period-count').text('Period: '+period);
+  $('#count-down-timer').text('0:'+timer+' remaining');
+  $('#start-game-button').click(function(){
+    $('#start-game-button').hide();
+
+    // initialize scores
+    let ninjasScore = 0;
+    let piratesScore = 0;
+    $('#ninjas-score').text(ninjasScore);
+    $('#pirates-score').text(piratesScore);
+    $('#ninja-image, #ninjas-score').click(function(){
+      ninjasScore++;
+      $('#ninjas-score').text(ninjasScore);
+    });
+    $('#pirate-image, #pirates-score').click(function(){
+      piratesScore++;
+      $('#pirates-score').text(piratesScore);
+    });
+    
+    // get timer going
+    let timeRemaining = setInterval(function(){
+      if(timer>0){
+        timer--;
+      } 
+      else{
+        if(period === 4){
+          //end game
+          clearInterval(timeRemaining);
+          $('#staticBackdrop').modal('show');
+          $('#modal-title').text("Game Over");
+          $('#modal-body').text('');
+          if(ninjasScore === piratesScore){
+            $('#modal-body').text('The game is tied at '+ninjasScore+' points');
+          } 
+          else{
+            if(ninjasScore > piratesScore){
+              $('#modal-body').append('<img src="./images/ninja.jpg" alt="Ninja logo" style="width: 100px">');
+              $('#modal-body').append('The score is '+ninjasScore+' to '+piratesScore+'\n Ninjas Win!!!');
+            } 
+            else{
+              $('#modal-body').append('<img id="pirate-image" src="./images/pirate.png" alt="Pirate logo" style="width: 100px">');
+              $('#modal-body').append('The score is '+piratesScore+' to '+ninjasScore+'\n Pirates Win!!!');
+            };
+          };
+        // keep rolling through the periods
+        } 
+        else{
+          if(period === 3){
+            alert('The third period is over');
+            period++;
+            $('#period-count').text('Period: '+period);
+            timer = 15;
+          } 
+          else{
+            if(period === 2){
+              alert('it\'s Halftime!');
+              period++;
+              $('#period-count').text('Period: '+period);
+              timer = 15;
+            } 
+            else{
+              alert('The 1st period is over');
+              period++;
+              $('#period-count').text('Period: '+period);
+              timer = 15;
+            };
+          };
+        };
+      };
+      // make sure timer displays properly  
+      if(timer>9){
+        $('#count-down-timer').text('0:'+timer+' remaining');
+      } 
+      else{
+        $('#count-down-timer').text('0:0'+timer+' remaining');
+      };
+    }, 1000);
+  });
+});
